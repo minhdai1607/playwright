61c1121c3e29ab46fb0930a007386d44e46f3a8a
Ngo Minh Dai <ngothien3937@gmail.com>
2025-12-23 13:09:20 +0700

    fix 3 sites

--- tests/okx/check-balance1.spec.ts
+++ tests/okx/check-balance1.spec.ts
@@ -2,14 +2,14 @@ import { expect } from '@playwright/test';
 import { okxpage } from '@ultil/fixtures/okx';
 import { readWalletAddresses } from '@ultil/common.ts';
 
-const wallets2 = readWalletAddresses('wallet2');
+const wallets1 = readWalletAddresses('wallet1');
 
-for (const wallet2 of wallets2) {
+for (const wallet1 of wallets1) {
     okxpage(
-        `Check balance of wallet mot ${wallet2}`,
+        `Check balance of wallet mot ${wallet1}`,
         { annotation: [{ type: 'id', description: 'T0002' }] },
         async ({ okx }) => {
-            await okx.okxPortfolioPage.verifyBalanceOfWallet(wallet2);
+            await okx.okxPortfolioPage.verifyBalanceOfWallet(wallet1);
             await expect(okx.okxPortfolioPage.noRecordsFoundText).toHaveText('No records found in the last 90 days');
         }
     );
